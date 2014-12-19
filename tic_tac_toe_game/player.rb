a657a1a216b491333f34251f0318a2178dd1347c
Anderson Mills <nodog@konfuzo.net>
2014-12-19 00:26:00 -0800

    Added random first move, and move choice output.

--- tic_tac_toe_game/player.rb
+++ tic_tac_toe_game/player.rb
@@ -7,9 +7,23 @@ class Player
     puts "Player #{mark} initialized."
   end
 
+  def empty_board?(board)
+    if (9 == board.board_state.chars.to_a.count('-')) then
+      return true 
+    else
+      return nil
+    end
+  end
+
   def choose_move(board, winning_combos)
     open_positions = board.all_open_positions
     puts "Considering the following positions #{open_positions}."
+    
+    if empty_board?(board)
+      puts "Random move for #{@mark}."
+      return open_positions.sample 
+    end
+
     # can I win
     move = choose_winning_move_if_available(open_positions, board, winning_combos)
     if move
@@ -36,6 +50,11 @@ class Player
       return move
     end
     # can I create a potential win (one mare with two empty spaces)
+    move = choose_potential_winning_move(open_positions, board, winning_combos)
+    if move
+      return move
+    end
+
     #
     # can I prevent a potential win
     #
@@ -50,7 +69,7 @@ class Player
       winning_combos.each do |combo|
         if combo.include? position then
           if (2 == board.board_state.chars.to_a.values_at(*combo).count(@mark)) then
-            puts "Found a winning move for #{@mark}."
+            puts "Found a winning move for #{@mark}, #{position}."
             return position
           end
         end
@@ -64,7 +83,7 @@ class Player
       winning_combos.each do |combo|
         if combo.include? position then
           if (2 == board.board_state.chars.to_a.values_at(*combo).reject{|c| c == @mark || c == '-'}.size) then
-            puts "Found a blocking move for #{@mark}."
+            puts "Found a blocking move for #{@mark}, #{position}."
             return position
           end
         end
@@ -79,7 +98,7 @@ class Player
         if combo.include? position then
           if (1 == board.board_state.chars.to_a.values_at(*combo).count(@mark)) \
              && (2 == board.board_state.chars.to_a.values_at(*combo).count('-')) then
-            puts "Found a probable winning move for #{@mark}."
+            puts "Found a probable winning move for #{@mark}, #{position}."
             return position
           end
         end
@@ -94,7 +113,7 @@ class Player
         if combo.include? position then
           if (1 == board.board_state.chars.to_a.values_at(*combo).reject{|c| c == @mark || c == '-'}.size) \
              && (2 == board.board_state.chars.to_a.values_at(*combo).count('-')) then
-            puts "Blocked a probable winning move for #{@mark}."
+            puts "Blocked a probable winning move for #{@mark}, #{position}."
             return position
           end
         end
@@ -103,4 +122,18 @@ class Player
     nil
   end
 
+  def choose_potential_winning_move(open_positions, board, winning_combos)
+    open_positions.each do |position|
+      winning_combos.each do |combo|
+        if combo.include? position then
+          if (3 == board.board_state.chars.to_a.values_at(*combo).count('-')) then
+            choice = combo.sample
+            puts "Found a potential winning move for #{@mark}, #{choice}."
+            return choice
+          end
+        end
+      end
+    end
+    nil
+  end
 end
