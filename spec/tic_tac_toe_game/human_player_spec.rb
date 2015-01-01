73eeb05d62fd2836787cb29a32074e17abec7a9c
Anderson Mills <anderson@puppetlabs.com>
2014-12-31 17:15:30 -0800

    Trying to update the language of the tests.

--- spec/tic_tac_toe_game/human_player_spec.rb
+++ spec/tic_tac_toe_game/human_player_spec.rb
@@ -2,12 +2,12 @@ require_relative '../../tic_tac_toe_game/human_player'
 
 describe HumanPlayer do
 
-  it 'should specify the mark when initialized' do
+  it 'specifies the mark when initialized' do
     a_player = HumanPlayer.new('m')
     expect(a_player.mark).to eq('m')
   end
 
-  it 'should choose a move like a human' do
+  it 'chooses a move like a human' do
     stub_board = instance_double("Board", :all_open_positions => [0, 1])
     a_player = HumanPlayer.new('m')
     # hunh, the following line is magic to me. :P
