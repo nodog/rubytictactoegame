8e565d90869609ad65fc15d5891dee0ddecb2bc5
Anderson Mills <anderson@puppetlabs.com>
2015-01-13 10:22:40 -0800

    Removed badly formed test in tic_tac_toe_game_spec

--- spec/tic_tac_toe_game_spec.rb
+++ spec/tic_tac_toe_game_spec.rb
@@ -5,9 +5,21 @@ describe TicTacToeGame do
     expect(TicTacToeGame.new).to_not eq(nil)
   end
 
-  it 'allow one move' do
-    a_tic_tac_toe_game = TicTacToeGame.new
-    expect(a_tic_tac_toe_game).to receive :advance_player
-    a_tic_tac_toe_game.allow_one_move
-  end
+#   it 'allows one move' do
+#     a_tic_tac_toe_game = TicTacToeGame.new
+# 
+#     mock_board = double("Board")
+#     mock_player = double("RuleBasedPlayer")
+# 
+#     a_tic_tac_toe_game
+# 
+#     allow_any_instance_of(HumanPlayer).to receive(:mark).twice.and_return('X')
+#     allow_any_instance_of(RuleBasedPlayer).to receive(:mark).and_return('X')
+#     allow_any_instance_of(HumanPlayer).to receive(:choose_move).with(any_args()).and_return(4)
+#     allow_any_instance_of(RuleBasedPlayer).to receive(:choose_move).with(any_args()).and_return(4)
+#     allow_any_instance_of(Board).to receive(:add_mark).with('X', 4)
+#     allow_any_instance_of(Board).to receive(:pretty_board).and_return('fake-pretty-board')
+#     expect(a_tic_tac_toe_game).to receive(:advance_player)
+#     a_tic_tac_toe_game.allow_one_move
+#   end
 end
