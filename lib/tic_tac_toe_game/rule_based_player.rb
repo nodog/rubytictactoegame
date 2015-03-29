5a50fb5fa478bb7825b4e67051b5f4cbd65e23bb
Anderson Mills <nodog@konfuzo.net>
2015-03-28 23:32:29 -0700

    Added special rule for first move in corner

--- lib/tic_tac_toe_game/rule_based_player.rb
+++ lib/tic_tac_toe_game/rule_based_player.rb
@@ -2,6 +2,8 @@ class RuleBasedPlayer
 
   PREFERRED_ORDER = [4, 0, 2, 8, 6, 1, 5, 7, 3] 
   PREFERRED_WINNING_COMBOS = [[4,0,8], [4,2,6], [0,2,1], [4,3,5], [6,8,7], [0,6,3], [4,1,7], [2,8,5]]
+  OUTER_RING = [0, 1, 2, 3, 5, 6, 7, 8]
+  CORNERS = [0, 2, 8, 6]
 
   attr_reader :mark
 
@@ -23,6 +25,7 @@ class RuleBasedPlayer
 
     move = move \
         || choose_winning_move_if_available(open_positions, board) \
+        || choose_adjacent_side_if_opponet_first_outer_ring_is_corner(open_positions, board) \
         || prevent_winning_move_with_guaranteed_win(open_positions, board) \
         || prevent_winning_move_with_probable_win(open_positions, board) \
         || prevent_winning_move_with_possible_win(open_positions, board) \
@@ -47,6 +50,21 @@ class RuleBasedPlayer
             return position
           end
         end
+      end
+    end
+    nil
+  end
+
+  def choose_adjacent_side_if_opponet_first_outer_ring_is_corner(open_positions, board)
+    # is the outer ring open except for one opponent move
+    if (1 == board.board_state.chars.to_a.values_at(*OUTER_RING).reject{|c| c == '-'}.size) \
+        && ( 0 == board.board_state.chars.to_a.values_at(*OUTER_RING).count(@mark))
+      # is the one opponent move in a corner
+      if (1 == board.board_state.chars.to_a.values_at(*CORNERS).reject{|c| c == '-'}.size) 
+        # move adjacent on side
+        index_in_corners = board.board_state.chars.to_a.values_at(*CORNERS).find_index {|c| c!= '-'} 
+        adjacent_on_side = CORNERS[ index_in_corners ] < 4 ? 1 : 7 
+        return adjacent_on_side
       end
     end
     nil
