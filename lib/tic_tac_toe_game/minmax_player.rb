bf483e0eaee68c18b3a9819cbf095cdf750cafc4
Anderson Mills <nodog@konfuzo.net>
2015-03-30 00:25:01 -0700

    Max for X and min for O seems right.
    But there are still problems.

--- lib/tic_tac_toe_game/minmax_player.rb
+++ lib/tic_tac_toe_game/minmax_player.rb
@@ -28,12 +28,13 @@ class MinmaxPlayer
     puts board_string
     mark_p1 = TicTacToeGame::PLAYER_1_MARK 
     move_values = @hash_of_states[board_string.to_sym][:move_value]
-    #if mark_p1 == @mark
+    puts move_values
+    if mark_p1 == @mark
       desired_move_value = move_values.compact.max
-    #else
-    #  desired_move_value = move_values.compact.min
-    #end
-    desired_move_indices = move_values.each_index.select{|i| !move_values[i].nil? && move_values[i] == desired_move_value}
+    else
+      desired_move_value = move_values.compact.min
+    end
+    desired_move_indices = move_values.each_index.select{|i| move_values[i] == desired_move_value}
     return desired_move_indices.sample
   end
 
